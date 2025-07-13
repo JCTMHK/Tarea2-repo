549ad79bdc3385a24a02af2b61f162afe7ea1119
jseba <jsebastian_cantos@hotmail.com>
2025-07-13 00:44:18 -0400

    Added process_2 implementation in python

---
+++ nextflow/scripts/process_2.py
@@ -0,0 +1,68 @@
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction
+import os
+import re
+## Read sequences files
+
+# # Implementation 1
+# # Describes every sequences in every file
+# for file in os.listdir("../DATA"):
+#     print("----------------------------------------")
+#     print(file)
+#     print("----------------------------------------")
+#     curFile=SeqIO.to_dict(SeqIO.parse(f"../DATA/{file}", "fasta"))
+#     num_seq=len(curFile)
+#     print("Number of sequences in file:", num_seq)
+#     totalSeqLen=0
+#     count=1
+#     for key in curFile:
+#             seqLen=len(curFile[key].seq)
+#             GCPercent=gc_fraction(curFile[key].seq)*100
+#             totalSeqLen+=seqLen
+#             print(f"File,SeqName,SeqLen,GC-Percent,")
+#             print(f"\t{count}. Name:",key)
+#             print("\t\tSeq Len:", seqLen)
+#             print(f"\t\tGC content (%): {GCPercent:.2f}")
+#             count+=1
+#     print("Total Seq Len in file:", totalSeqLen)
+#     print("----------------------------------------")
+
+
+# Implementation 2
+# Describes only file
+print(f"File,NumSeq,TotalSeqLen,TotalGC-Percent")
+for file in os.listdir("../DATA"):
+    curFile=SeqIO.to_dict(SeqIO.parse(f"../DATA/{file}", "fasta"))
+    num_seq=len(curFile)
+    totalSeqLen=0
+    gcSum=0
+    count=1
+    for key in curFile:
+            seqLen=len(curFile[key].seq)
+            gCon = len(re.findall("G", str(curFile[key].seq),re.IGNORECASE))
+            cCon = len(re.findall("C", str(curFile[key].seq),re.IGNORECASE))
+            gcSum+=(gCon+cCon)
+            GCPercent=gc_fraction(curFile[key].seq)*100
+            totalSeqLen+=seqLen
+    totalGC=(gcSum/totalSeqLen)*100
+    print(f"{file},{num_seq},{totalSeqLen},{totalGC:.2f}")
+
+
+
+# # Implementation 3
+# # Describe only file with GC kmer count
+# print(f"File,NumSeq,TotalSeqLen,TotalGC-Percent")
+# for file in os.listdir("../DATA"):
+#     curFile=SeqIO.to_dict(SeqIO.parse(f"../DATA/{file}", "fasta"))
+#     num_seq=len(curFile)
+#     totalSeqLen=0
+#     gcKmerSum=0
+#     count=1
+#     for key in curFile:
+#             seqLen=len(curFile[key].seq)
+#             gcCon = len(re.findall("GC", str(curFile[key].seq),re.IGNORECASE))
+#             totalSeqLen+=seqLen
+#             gcKmerSum+=gcCon
+#     totalGC=(gcKmerSum/totalSeqLen)*100
+   
+#     print(f"{file},{num_seq},{totalSeqLen},{totalGC:.2f}")
