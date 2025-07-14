9a851ced622390ab964cf06e2d0e8989fe95e2b5
jseba <jsebastian_cantos@hotmail.com>
2025-07-13 23:56:24 -0400

    Implementation of process 5

--- nextflow/scripts/process_3.py
+++ nextflow/scripts/process_3.py
@@ -27,27 +27,48 @@ refSeq =ref[choiceRef]
 choiceQuery = random.choice(list(query.keys()))
 querySeq =query[choiceQuery]
 
+
+
 del ref
 del query
-del choiceRef
-del choiceQuery
+# del choiceRef
+# del choiceQuery
 
 # Generate kmer dictionaries
 k=10
 dict1 = generate_kmers_with_positions(refSeq.seq,k)
-dict2 = generate_kmers_with_positions(refSeq.seq,k)
+dict2 = generate_kmers_with_positions(querySeq.seq,k)
 
 # Match kmers
 matches=find_common_kmers(dict1,dict2)
 
-outName=f"ref-{refSeq.name}-v-query-{querySeq.name}-{k}_kmers.pkl"
+outName=f"ref-{refSeq.name}-v-query-{querySeq.name}-{k}_kmers"
 
 print(matches)
 
+
+# Parameters of kmers index
+seqParm={}
+seqParm[choiceRef]=len(refSeq)
+seqParm[choiceQuery]=len(querySeq)
+seqParm["kmer_size"]=k
+print(seqParm)
+
+
 # Save the dictionary to a file
-save_dir="../RESULTS"
-with open(outName, 'wb') as file:
+# Save kmer index dict to file
+with open(f"{outName}.pkl", 'wb') as file:
     pickle.dump(matches, file)
+
+# Save parameters dict to file
+with open(f"{outName}_parm.pkl", 'wb') as file:
+    pickle.dump(matches, file)
+
+# Save selected sequences to file
+with open(f"ref-{refSeq.name}_seq.pkl", 'wb') as file:
+    pickle.dump(refSeq, file)
+with open(f"query-{querySeq.name}_seq.pkl", 'wb') as file:
+    pickle.dump(querySeq, file)
 
 
 
